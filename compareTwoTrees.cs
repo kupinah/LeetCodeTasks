49052b82f72fa77ba5c4c9baabd0372deee6a2e1
kupinah <hk8302@student.uni-lj.si>
2021-02-12 22:03:30 +0100

    Check if Two Trees Are Equal or Not - easy

---
+++ compareTwoTrees.cs
@@ -0,0 +1,7 @@
+class comparetwotrees{
+    public bool IsSameTree(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if(p != null && q != null) return (p.val == q.val && IsSameTree(p.left, q.left) && IsSameTree(p.right, q.right));
+        return false;
+    }
+}
